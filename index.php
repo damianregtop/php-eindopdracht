1ed5a9a6c8e6bdcdacde899198b3788a7f072f77
damian regtop <damianregtop@gmail.com>
2020-01-15 13:59:18 +0100

    backbone of frontend

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
 <html>
+  <head>
+    <title>rock paper sciccors</title>
+
+    <meta charset="utf-8">
+    <meta lang="en">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <h1>welcome to rock paper sciccors</h1>
+    <p>please select rock, paper or sciccors:</p>
+    <form action="process.php" method="post">
+      <input type="submit" name="submit" value="rock">
+      <input type="submit" name="submit" value="paper">
+      <input type="submit" name="submit" value="sciccors">
+      <h4>reminder</h4>
+      <p>rock beats sciccors </br>
+        sciccors beats paper </br>
+        paper beats rock </p>
+    </form>
+  </body>
 </html>
